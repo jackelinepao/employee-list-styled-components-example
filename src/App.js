ed84e87bf4faef78fb3dbc7fac755800a35ff216
Jose Tobar <josectobar@gmail.com>
2022-02-08 17:12:54 -0700

    moved navigator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import ContextProvider from "./EmployeeContext";
-import { Form } from "./Form";
-import { List } from "./List";
 import "./App.css";
+import { MainNavigator } from "./navigation/MainNavigator";
 import "antd/dist/antd.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <ContextProvider>
-        <Router>
-          <nav>
-            <Link to="/add-employee">Add Employee</Link> |{" "}
-            <Link to="/employee-list">Employee List</Link>
-          </nav>
-          <Routes>
-            <Route path="/add-employee" element={<Form />} />
-            <Route path="/employee-list" element={<List />} />
-          </Routes>
-        </Router>
+        <MainNavigator />
       </ContextProvider>
     </div>
   );
---
+++ src/navigation/MainNavigator.js
@@ -0,0 +1,20 @@
+import { Form } from "./Form";
+import { List } from "./List";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function MainNavigator() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/add-employee">Add Employee</Link> |{" "}
+        <Link to="/employee-list">Employee List</Link>
+      </nav>
+      <Routes>
+        <Route path="/add-employee" element={<Form />} />
+        <Route path="/employee-list" element={<List />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export { MainNavigator };
